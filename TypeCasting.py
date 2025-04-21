b910ece5dc7c89e72a05b346503224653bab9933
123jasamit <jasamitsaha@gmail.com>
2025-04-21 17:34:36 +0100

    Add files via upload

---
+++ TypeCasting.py
@@ -0,0 +1,20 @@
+name = "cat"
+age = 6
+it_student = True
+weight = 10.6
+print("name: ",name)
+print("data type of name is: ",type(name))
+print("age: ",age)
+print("data type of age is:",type(age))
+print("is student: ",type(it_student))
+print("weight: ",weight)
+print (Data Type Of Weight Is)
+#Typecasting
+print("\n\nTypecasting")
+age = str(age)
+print("age: ",age)
+print("data type of age is: ",type(age))
+weight = int(weight)
+print("weight: ",weight)
+print("data type of weight is: ",type(weight))
+
