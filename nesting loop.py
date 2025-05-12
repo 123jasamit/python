3a454ffd42997ece7157852d92b6017fedaff6e5
123jasamit <jasamitsaha@gmail.com>
2025-05-12 17:24:18 +0100

    Add files via upload

---
+++ nesting loop.py
@@ -0,0 +1,19 @@
+#take input of the student is he can attend the exam or not
+medicinal_cause=input("did you have a medical couse Y oar N")
+#take input of the attendence
+atten=int(input("enter the attendence of the student":))
+
+#checking the users input predicting output accordingly
+ if medical_cause == "y": #checking the condition 1:
+ print("you are allowed")
+else:
+ if atten>=75: # checking the condition 2:
+ print("allowed")
+else:
+ print("not allowed")
+
+     
+ 
+ 
+    
+
